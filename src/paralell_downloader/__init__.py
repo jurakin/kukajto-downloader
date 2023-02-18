ff5967d56bdb4266e2b68f908254ccf58854c36a
Jaja <48247542+jurakin@users.noreply.github.com>
2023-02-18 18:12:47 +0100

    Delete src/paralell_downloader directory

--- src/paralell_downloader/__init__.py
+++
@@ -1,9 +0,0 @@
-from downloader import ParalellDownloader
-from downloader import ParalellDownloaderError
-
-__all__ = [
-    "ParalellDownloader",
-    "ParalellDownloaderError",
-]
-
-__version__ = "1.0"
